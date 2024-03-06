886a22edfb72929454a0325f92c6ea9975fce3df
jiopuk2 <162070804+jiopuk2@users.noreply.github.com>
2024-03-06 19:52:36 +0800

    Add files via upload

---
+++ 林/aaa.py
@@ -0,0 +1,10 @@
+def Han_No_Ta(n, i, j, k):
+    if n == 1:
+        print(i, "->", k)
+        return
+    Han_No_Ta(n - 1, i, k, j)
+    Han_No_Ta(1, i, j, k)
+    Han_No_Ta(n - 1, j, i, k)
+
+
+Han_No_Ta(7, "1", "2", "3")
